b58d3453900678c0bc1e8ffee99ccbcd8e7672f0
Serge <example@gmail.com>
2020-03-10 17:43:14 +0300

    lesson 2 (added loops and conditions)

--- script.js
+++ script.js
@@ -10,9 +10,38 @@ let appData = {
     income: [],
     savings: false,
 };
-let answer_1 = prompt("Введите обязательную статью расходов в этом месяце", "");
-let answer_2 = +prompt("Во сколько обойдется?", "");
+for (let i=0; i<2; i++) {
+let answer_1 = prompt("Введите обязательную статью расходов в этом месяце", ""), 
+answer_2 = +prompt("Во сколько обойдется?", "");
+if ((typeof(answer_1))=='string' && answer_1 != null && answer_1 != "" && answer_1 != undefined && answer_2 != null && answer_2 != "" && answer_2 != undefined) {
 appData.expenses[answer_1] = answer_2;
-alert (money/30);
+} else console.log("error in input data");
+}
+
+/*
+let i = 0;
+do {
+    let answer_1 = prompt("Введите обязательную статью расходов в этом месяце", ""), 
+    answer_2 = +prompt("Во сколько обойдется?", "");
+    if ((typeof(answer_1))=='string' && answer_1 != null && answer_1 != "" && 
+    answer_1 != undefined && answer_2 != null && answer_2 != "" && answer_2 != undefined) {
+    appData.expenses[answer_1] = answer_2;
+    i++;
+    }}
+    while (i<2);
+*/
+/*   let i = 0;
+    while (i<2) {
+    let answer_1 = prompt("Введите обязательную статью расходов в этом месяце", ""), 
+    answer_2 = +prompt("Во сколько обойдется?", "");
+    if ((typeof(answer_1))=='string' && answer_1 != null && answer_1 != "" && 
+    answer_1 != undefined && answer_2 != null && answer_2 != "" && answer_2 != undefined) {
+    appData.expenses[answer_1] = answer_2;
+    i++;
+    }
+}
+*/  
+
+alert ("Ежедневный бюджет " + money/30);
 console.log (appData.expenses);
 console.log (appData);
